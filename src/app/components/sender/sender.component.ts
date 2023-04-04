1d561cfb0fbef078e7f47cee705fdbacb81a06e1
krymniak <krymnyak@gmail.com>
2023-04-04 13:28:37 +0300

    create custom validator for sender component

--- src/app/components/sender/sender.component.ts
+++ src/app/components/sender/sender.component.ts
@@ -1,35 +1,44 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
 import { User } from 'src/app/interfaces';
 import { CtsService } from 'src/app/services/cts.servise';
+import { jsonArrayValidator } from './json.validator';
 
 @Component({
 	selector: 'app-sender',
 	templateUrl: './sender.component.html',
 	styleUrls: ['./sender.component.scss']
 })
-export class SenderComponent {
+export class SenderComponent implements OnInit{
 
-	@ViewChild('jsonInput') jsonInput: any;
+	form!: FormGroup;
+
 	users$ = this.service.users$.asObservable()
 	displayedUsers$ = new BehaviorSubject<User[] | null>([])
 	constructor(private service: CtsService) {
-		this.displayedUsers$.next(this.service.users)
+  }
+
+	ngOnInit(): void {
+		this.form = new FormGroup({
+			jsonArray: new FormControl(JSON.stringify(this.service.users), [jsonArrayValidator()])
+		})
 	}
+	
 
 	getUsers() {
-		const jsonArray = JSON.parse(this.jsonInput.nativeElement.value)
+		const jsonArray = JSON.parse(this.form.value.jsonArray)
 		const users = jsonArray
 		this.service.users$.next(users)
 		this.displayedUsers$.next([])
-		this.jsonInput.nativeElement.value = []
+		this.form.setValue({jsonArray: []})
 		this.service.lastUser$.next(users[users.length - 1])
 	}
 	createJson() {
 		const users = this.service.users$.getValue()
 		this.displayedUsers$.next(users)
 		const myJsonString = JSON.stringify(users);
-		this.jsonInput.nativeElement.value = myJsonString
+		this.form.setValue({jsonArray: myJsonString})
 	}
 
 }
